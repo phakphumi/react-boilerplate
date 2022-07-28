4c3ff1dd36a278397be13beccac2ddf27485ecf3
Pakpoom Thaweesitthichat <pakpoom.thawee@gmail.com>
2022-07-28 13:13:32 +0700

    setup prettier and config eslint for minimize code complexity

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,34 +1,29 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es2021': true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
   },
-  'extends': [
+  extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  'plugins': [
-    'import',
-    'react',
-    'unused-imports',
-    '@typescript-eslint',
-  ],
-  'rules': {
+  plugins: ['import', 'react', 'unused-imports', '@typescript-eslint'],
+  rules: {
     '@typescript-eslint/no-empty-interface': 'warn',
     '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-shadow': 'error',
-    'comma-dangle': ['error', 'always-multiline'],
     'import/no-duplicates': 'warn',
     'import/no-useless-path-segments': 'warn',
     'import/order': [
@@ -48,7 +43,7 @@ module.exports = {
         groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'type'],
       },
     ],
-    'indent': ['error', 2],
+    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'],
     'no-console': [
       'warn',
@@ -56,16 +51,47 @@ module.exports = {
         allow: ['error'],
       },
     ],
-    'quotes': ['error', 'single', {
-      avoidEscape: true,
-    }],
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+      },
+    ],
     'react/no-unescaped-entities': 'error',
     'react/prop-types': 'off',
-    'semi': ['error', 'always'],
+    semi: ['error', 'always'],
     'unused-imports/no-unused-imports': 'error',
     'unused-imports/no-unused-vars': [
       'warn',
-      { 'vars': 'all', 'varsIgnorePattern': '^_', 'args': 'after-used', 'argsIgnorePattern': '^_' },
+      {
+        vars: 'all',
+        varsIgnorePattern: '^_',
+        args: 'after-used',
+        argsIgnorePattern: '^_',
+      },
     ],
+    // Rules regarding minimize code complexity
+    'max-lines': [
+      'warn',
+      {
+        max: 200,
+        skipBlankLines: true,
+        skipComments: true,
+      },
+    ],
+    'max-lines-per-function': [
+      'warn',
+      {
+        max: 50,
+        skipBlankLines: true,
+        skipComments: true,
+      },
+    ],
+    'max-nested-callbacks': ['warn', { max: 2 }],
+    'max-depth': ['warn', { max: 3 }],
+    'max-params': ['warn', { max: 2 }],
+    'no-param-reassign': ['error'],
+    complexity: ['warn', { max: 5 }],
   },
 };
